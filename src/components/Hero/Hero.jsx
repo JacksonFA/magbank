2bd80ccfe4eb758343f2be6f9d667a5a3567c6bf
JacksonFA <jackson.f205@gmail.com>
2024-02-21 19:55:38 -0300

    feat(hero): ajusta o componente Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,23 @@
+import { Button, Col, Container, Image, Row } from 'react-bootstrap'
+import heroBackground from '../../assests/hero.png'
+import heroLogo from '../../assests/logo-hero.png'
+import './Hero.sass'
+
 export function Hero() {
   return (
-    <div className="w-full bg-purple-200 py-sm">
-      <img src="./src/assests/hero.png" alt="Imagem Hero" className="img-fluid w-full"></img>
-      <img src="./src/assests/logo-hero.png" alt="Imagem Logo Hero" className="img-fluid"></img>
-      <p className="bg-red">Pague suas contas pelo nosso</p>
+    <div className="relative">
+      <Image src={heroBackground} alt="Imagem Hero" className="backgroundImg" />
+      <Container className="relative py-2xl text-center">
+        <Row>
+          <Col lg>
+            <Image src={heroLogo} alt="Imagem Logo Hero" className="" />
+          </Col>
+          <Col lg>
+            <p className="my-xl font-xs color-white">Pague suas contas pelo nosso APP</p>
+            <Button variant="outline-light p-md">Abra sua conta</Button>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import { Header } from '../components/Header/Header'
+import { Hero } from '../components/Hero/Hero'
 
 export default function Home() {
   return (
     <main className="dark-theme h-max">
       <Header />
+      <Hero />
     </main>
   )
 }
