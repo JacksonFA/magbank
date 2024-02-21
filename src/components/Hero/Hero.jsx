f7902f56be52f66e0910527e4ecb20fa0466f6da
miltonralves <miltonralves@gmail.com>
2024-02-21 19:17:23 -0300

    commit da branch hero

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,9 @@
+export function Hero() {
+  return (
+    <div className="w-full bg-purple-200 py-sm">
+      <img src="./src/assests/hero.png" alt="Imagem Hero" className="img-fluid w-full"></img>
+      <img src="./src/assests/logo-hero.png" alt="Imagem Logo Hero" className="img-fluid"></img>
+      <p className="bg-red">Pague suas contas pelo nosso</p>
+    </div>
+  )
+}
